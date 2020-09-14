a5bd1524f351a6f46d7350c87554b5daaae350f1
Meng Xu <meng_xu@apple.com>
2020-09-13 17:03:27 -0700

    Enable split txn for testing

--- fdbclient/Knobs.cpp
+++ fdbclient/Knobs.cpp
@@ -222,7 +222,7 @@ void ClientKnobs::initialize(bool randomize) {
 
 	// Large transaction
 	init( LARGE_TRANSACTION_CRITERIA,       1024 * 1024 ); if( randomize && BUGGIFY ) LARGE_TRANSACTION_CRITERIA = 100; // 1MB // TODO: Make this random value in a range
-	init( TRANSACTION_SPLIT_MODE,                   0b0 ); // 0b0: disable split mode; 0b001 enable split mode; TODO: Buggify to a different mode
+	init( TRANSACTION_SPLIT_MODE,                   0b1 ); // 0b0: disable split mode; 0b001 enable split mode; TODO: Buggify to a different mode
 	
 	//fdbcli		
 	init( CLI_CONNECT_PARALLELISM,                  400 );
